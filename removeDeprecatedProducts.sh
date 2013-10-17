59f3207a07ebc94c574d29b86e3426da7e15a95d
Ryan Manly <ryan.manly@gmail.com>
2013-10-17 09:17:02 -0500

    style changes
    move 'then' and 'do' to first line of statement

--- removeDeprecatedProducts.sh
+++ removeDeprecatedProducts.sh
@@ -5,23 +5,20 @@
 # Place this script in the same folder as repoutil, otherwise it will not work properly.
 
 # Sanity Check.  Makes sure a branch is specified.
-if [[ -z $1 ]]
-then
+if [[ -z $1 ]]; then
     echo "Please specify a branch"
     exit 1
 fi
 
 # Iterates through the output that shows deprecated items in a given branch.
 i=0
-for item in $(./repoutil --list-branch=${1} | awk '/Deprecated/{ print $1 }')
-do
+for item in $(./repoutil --list-branch=${1} | awk '/Deprecated/{ print $1 }'); do
     deprecatedProducts[$i]=$item
     let i++
 done
 
 # Removes deprecated products
-for product in "${deprecatedProducts[@]}"
-do
+for product in "${deprecatedProducts[@]}"; do
     ./repoutil --remove-product=$product $1
     echo "Removed ${product}"
 done
