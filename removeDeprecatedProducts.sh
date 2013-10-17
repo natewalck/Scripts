b847cbe1af52030aec6901b512d17411c93ace65
Ryan Manly <ryan.manly@gmail.com>
2013-10-17 09:14:46 -0500

    change spacing from 8 to 4 spaces

--- removeDeprecatedProducts.sh
+++ removeDeprecatedProducts.sh
@@ -7,21 +7,21 @@
 # Sanity Check.  Makes sure a branch is specified.
 if [ -z $1 ]
 then
-        echo "Please specify a branch"
-        exit 1
+    echo "Please specify a branch"
+    exit 1
 fi
 
 # Iterates through the output that shows deprecated items in a given branch.
 i=0
 for item in $(./repoutil --list-branch=${1} | awk '/Deprecated/{ print $1 }')
 do
-        deprecatedProducts[$i]=$item
-        let i++
+    deprecatedProducts[$i]=$item
+    let i++
 done
 
 # Removes deprecated products
 for product in "${deprecatedProducts[@]}"
 do
-        ./repoutil --remove-product=$product $1
-        echo "Removed ${product}"
+    ./repoutil --remove-product=$product $1
+    echo "Removed ${product}"
 done
